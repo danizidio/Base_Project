e146afddf31a427bf4c7a6f5b40fe2f5af03b08e
Daniel Amorim <38110599+danizidio@users.noreply.github.com>
2023-04-16 19:38:24 -0300

    @UPDATE - InputHandler and Button arts

---
+++ Assets/Scripts/UI/InputHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] PlayerInput _input;
+
+    [SerializeField] Sprite _keyboardBtn, _controllerBtn;
+
+    void Start()
+    {
+        GameObject obj = GameObject.FindGameObjectWithTag("Player");
+
+        _input = obj.GetComponent<PlayerInput>();
+
+        _input.controlsChangedEvent.AddListener(ChangeControlUI);
+
+        _input.controlsChangedEvent?.Invoke(_input);
+    }
+
+    public void ChangeControlUI(PlayerInput _p)
+    {
+        if (_p.currentControlScheme.Equals("Controller"))
+        {
+
+
+        }
+        if (_p.currentControlScheme.Equals("KeyBoard"))
+        {
+
+        }
+
+        print(_p.currentControlScheme);
+    }
+}
