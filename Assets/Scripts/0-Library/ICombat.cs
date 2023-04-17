4cc0fa38af212d404b5817bd639cb288cd4c9ec6
Daniel Amorim <38110599+danizidio@users.noreply.github.com>
2023-04-16 21:28:40 -0300

    @UPDATE - Removing unnecessary namespaces

--- Assets/Scripts/0-Library/ICombat.cs
+++ Assets/Scripts/0-Library/ICombat.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ICombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void TakeDamage(float dmg)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
